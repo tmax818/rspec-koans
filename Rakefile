2fbeb028f7083e7c416313bb471180c4b8feb35b
tmax818 <tylermaxwell661@gmail.com>
2021-04-20 18:52:38 -0700

    add rakefile

---
+++ Rakefile
@@ -0,0 +1,14 @@
+
+
+
+task :default => :test
+
+desc "some description.."
+task :test do
+  sh "rspec"
+end
+
+desc "some other description"
+task :test_html do
+  sh "rspec --format html --out index.html"
+end
--- spec/koanspecs/01about_arrays_spec.rb
+++ spec/koanspecs/01about_arrays_spec.rb
@@ -1,9 +1,36 @@
 describe Array do
     let(:__) {nil}
 
-    it "tests creating arrays" do
-        empty_array = Array.new
-        expect(Array).to be(empty_array.class)
-        expect(__).to be(subject.class), "same test using rspec tools"
+    context "-- Please meditate on the following:" do
+        
+        it "test creating arrays" do
+            empty_array = Array.new
+            expect(__).to eq(empty_array.class)
+            expect(__).to eq(subject.class), "same test using rspec tools"
+            expect(__).to eq(subject.size)
+        end
+
+        it "test array literals" do
+            array = Array.new 
+            expect(__).to eq(array)
+            array[0] = 1
+            expect(__).to eq(array)
+            array[1] = 2
+            expect([1, __]).to eq(array)
+            array << 333
+            expect(__).to eq(array)
+        end
+
+        it "test accessing array elements" do
+            array = [:peanut, :butter, :and, :jelly]
+            expect(__).to eq(array[0])
+            expect(__).to eq(array.first)
+            expect(__).to eq(array[3])
+            expect(__).to eq(array.last)
+            expect(__).to eq(array[-1])
+            expect(__).to eq(array[-3])
+
+
+        end
     end
 end
