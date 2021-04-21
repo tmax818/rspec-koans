c9376b334a1866e66980a7f638a3881ed00ed334
tmax818 <tylermaxwell661@gmail.com>
2021-04-20 19:44:35 -0700

    finish arrays

--- spec/koanspecs/01about_arrays_spec.rb
+++ spec/koanspecs/01about_arrays_spec.rb
@@ -29,8 +29,53 @@ describe Array do
             expect(__).to eq(array.last)
             expect(__).to eq(array[-1])
             expect(__).to eq(array[-3])
+        end
 
+        it "test slicing arrays" do
+            array = [:peanut, :butter, :and, :jelly]
+            expect(__).to eq(array[0,1])
+            expect(__).to eq(array[0,2])
+            expect(__).to eq(array[2,2])
+            expect(__).to eq(array[2,20])
+            expect(__).to eq(array[4,0])
+            expect(__).to eq(array[4,100])
+            expect(__).to eq(array[5,0])
+        end
 
+        it "test_arrays_and_ranges" do
+            expect(__).to eq((1..5).class)
+            expect([1,2,3,4,5]).to eq(1..5), "use .not_to"
+            expect(__).to eq((1..5).to_a)
+            expect(__).to eq((1...5).to_a)
+        end
+
+        it "test slicing with ranges" do
+            array = [:peanut, :butter, :and, :jelly]
+            expect(__).to eq(array[0..2])
+            expect(__).to eq(array[0...2])
+            expect(__).to eq(array[2..-1])
+        end
+
+        it "test_pushing_and_popping_arrays" do
+            array = [1,2]
+            array.push(:last)
+
+            expect(__).to eq(array)
+
+            popped_value = array.pop
+            expect(__).to eq(popped_value)
+            expect(__).to eq(array)
+        end
+
+        it "test_shifting_arrays" do
+            array = [1,2]
+            array.unshift(:first)
+
+            expect(__).to eq(array)
+
+            shifted_value = array.shift
+            expect(__).to eq(shifted_value)
+            expect(__).to eq(array)
         end
     end
 end
